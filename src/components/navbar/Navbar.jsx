25d9c084f85f041207c308e143f36c6ba4ed6411
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2024-02-19 14:03:26 +0200

    finsh some of the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,14 @@
+import Link from "next/link";
+import Links from "./links/Links";
+
 const Navbar = () => {
     return ( 
-        <div>Navbar</div>
+        <div>
+         <div>Logo</div>         
+        <div>
+            <Links />
+         </div>
+        </div>
      );
 }
  
