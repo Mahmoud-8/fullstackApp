1205307b344446bcd8018be235856c33ed07852b
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2024-02-22 17:37:39 +0200

    finsh some style of the navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,25 @@
+import styles from './home.module.css';
+import Image from 'next/image';
+
 const Home = () => {
   return ( 
-    <div>
-      Homepage
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1> Creative  Web Developer </h1>
+        <p>Im a web developer specialized in creating responsive and interactive websites. Im passionate about creating beautiful and functional websites that help businesses grow. </p>
+        <div className={styles.buttons}>
+        <button className={styles.button}>See my work</button>
+          <button className={styles.button}>Contact Me</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt= "" fill className={styles.brandImg} />
+
+        </div>
+      </div>
+      <div className={styles.textContainer}>
+        <Image src="/hero.gif" alt= "" fill className={styles.heroImg} />
+       </div>
+
     </div>
    );
 }
