4cc503a225dfaf24cfa3745a0b26054af52f6f71
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2024-02-22 17:10:44 +0200

    finsh some style of the navbar

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,15 @@
+import styles from "./about.module.css";
+import Image from "next/image";
+
 const AboutPage = () => {
     return ( 
-        <div>AboutPage</div>
+        <div>
+            <div className={styles.imgContainer}>
+                <Image src="/about.png" alt="" fill/>
+
+            </div>
+        </div>
      );
 }
  
-export default AboutPage
-;
+export default AboutPage;
